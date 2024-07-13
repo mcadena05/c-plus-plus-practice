5ae84fb73519eab076022ff5b0a833039f125a4a
mcadena05 <cadenamarlynn@gmail.com>
2024-07-12 18:13:30 -0600

    constructors learning

--- classes-objects.cpp
+++ classes-objects.cpp
@@ -8,6 +8,20 @@ class Book { //blueprint of attributes
         string author;
         int pages;
 
+        Book (){ // constructor that takes in no arugments and creates default like attributes
+            title = 'no title';
+            author = 'no author';
+            pages = 0
+        }
+
+        Book(string aTitle, string aAuthor, int aPages) { // constructor = method to create class instance (object)
+        // constructor can take in arguments 
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+
+        }
+
     
 };
 
@@ -16,13 +30,16 @@ int main () {
     // create an object here
     // object == instance 
 
-    Book book1; // creating the actual instance
+    Book book1(); // creating the actual instance
 
     // Giving the object attributes
     book1.title = ' Harry Potter';
     book1.author = ' Jk Rowling';
     book1.pages = 500;
   
+
+    // creating an isntance using the constructor
+    Book book2 ("Lord of the Rings", "Tolkrin", 700);
 
     return 0;
 }
