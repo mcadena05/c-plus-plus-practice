877a65d9fccfc659237e3bf9d4fa3ca663f634fa
mcadena05 <cadenamarlynn@gmail.com>
2024-07-16 11:50:53 -0600

    Inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Chef {
-    public:
+    public:// super class
         void makeChicken() {
             cout<< "The chef makes chicken" << endl
         }
@@ -12,12 +12,25 @@ class Chef {
         void makeSpecialDish() {
             cout<< "The chef makes Korean Fried Chicken" << endl
         }
-}
+};
+
+class ItalianChef: public Chef{ // adding the public Chef allows for this class to inherite all the functions in the parent class
+    public: // subclass
+        void makePasta () {
+            cout << "The chef makes pasta" << endl;
+        }
+        void makeSpecialDish() { // we can override the original inherited method
+            cout<< "The chef makes Spagetti" << endl
+        }
+};
 
 int main(){
 
     Chef chef;
     chef.makeChicken();
+
+    ItalianChef italianChef;
+    italianChef.makeChicken();
 
     return 0;
 }
